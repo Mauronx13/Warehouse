5284f13866f9f25b33574fa2a4e6bc23067a8709
Maria Vargas Aponte <Mary@marias-imac.local>
2016-09-11 22:15:58 -0400

    Initial Commit

---
+++ Warehouse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Warehouse
+//
+//  Created by Maria Vargas Aponte on 9/11/16.
+//  Copyright © 2016 Maria Vargas Aponte. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
